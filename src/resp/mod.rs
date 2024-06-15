9e7eb98ed383875cae6fa960167acdd596e8dccf
hearsay316 <hearsay316@gmail.com>
2024-06-15 11:29:38 +0800

    feat: 定义 数据类型

--- src/resp/mod.rs
+++ src/resp/mod.rs
@@ -1,18 +1,118 @@
 use std::collections::{HashMap, HashSet};
 use anyhow::Result;
+use bytes::BytesMut;
+use std::ops::Deref;
+
+mod encode;
+mod decode;
+
+use encode::*;
+use decode::*;
+/*
+- 如何解析 Frame
+    - simple string: "+OK\r\n"
+    - error: "-Error message\r\n"
+    - bulk error: "!<length>\r\n<error>\r\n"
+    - integer: ":[<+|->]<value>\r\n"
+    - bulk string: "$<length>\r\n<data>\r\n"
+    - null bulk string: "$-1\r\n"
+    - array: "*<number-of-elements>\r\n<element-1>...<element-n>"
+        - "*2\r\n$3\r\nget\r\n$5\r\nhello\r\n"
+    - null array: "*-1\r\n"
+    - null: "_\r\n"
+    - boolean: "#<t|f>\r\n"
+    - double: ",[<+|->]<integral>[.<fractional>][<E|e>[sign]<exponent>]\r\n"
+    - map: "%<number-of-entries>\r\n<key-1><value-1>...<key-n><value-n>"
+    - set: "~<number-of-elements>\r\n<element-1>...<element-n>"
+ */
+pub trait RespEncode {
+    fn encode(self) -> Vec<u8>;
+}
+
+pub trait RespDecode {
+    fn decode(buf: Self) -> Result<RespFrame, String>;
+}
+
+impl RespDecode for BytesMut {
+    fn decode(buf: Self) -> Result<RespFrame, String> {
+        todo!()
+    }
+}
 
 pub enum RespFrame {
     SimpleString(SimpleString),
     Error(SimpleError),
     Integer(i64),
-    BulkString(Vec<u8>),
-    Array(Vec<RespFrame>),
-    Null,
-    NullArray,
+    BulkString(BulkString),
+    Array(RespArray),
+    Null(RespNull),
+    NullArray(RespNullArray),
     Boolean(bool),
     Double(f64),
-    Map(HashMap<String,RespFrame>),
-    Set(HashSet<RespFrame>),
+    Map(RespMap),
+    Set(RespSet),
 }
+
 pub struct SimpleString(String);
+
 pub struct SimpleError(String);
+
+pub struct RespNull;
+
+pub struct RespNullArray;
+
+pub struct RespArray(Vec<RespFrame>);
+
+pub struct BulkString(Vec<u8>);
+
+pub struct RespMap(HashMap<String, RespFrame>);
+
+pub struct RespSet(HashSet<RespFrame>);
+
+impl Deref for SimpleString {
+    type Target = String;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for SimpleError {
+    type Target = String;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for BulkString {
+    type Target = Vec<u8>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for RespArray {
+    type Target = Vec<RespFrame>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for RespMap {
+    type Target = HashMap<String, RespFrame>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for RespSet {
+    type Target = HashSet<RespFrame>;
+    fn deref(&self) -> &Self::Targe {
+        &self.0
+    }
+}
+
+impl SimpleString {
+    pub fn  new (s:impl Into<String>)->Self{
+        SimpleString(s.into())
+    }
+}
