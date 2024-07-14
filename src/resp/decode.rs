10a8cddfa4af2e58bcecc216295c7a87ae2997eb
hearsay316 <hearsay316@gmail.com>
2024-07-15 00:27:45 +0800

    feat: 1   封装 工具函数

--- src/resp/decode.rs
+++ src/resp/decode.rs
@@ -1,18 +1,8 @@
 
-use crate::{RespDecode, RespError, RespFrame, SimpleError, SimpleString};
+use crate::{RespArray, RespDecode, RespError, RespFrame, SimpleError, SimpleString};
 use bytes::BytesMut;
-// impl RespDecode for RespFrame {
-//     fn decode(buf: &mut BytesMut) -> Result<RespFrame, RespError> {
-//         let mut item = buf.iter().peekable();
-//         match item.peek() {
-//             Some(b'+') => {
-//                 todo!()
-//             }
-//             _ => todo!()
-//         }
-//         todo!()
-//     }
-// }
+const CRLF: &[u8] = b"\r\n";
+const CRLF_LEN: usize = CRLF.len();
 impl RespDecode for SimpleString {
     fn decode( buf: &mut BytesMut) -> Result<Self, RespError> {
         if buf.len() < 3 {
@@ -62,6 +52,59 @@ impl RespDecode for SimpleError {
         let s = String::from_utf8_lossy(&data[1..end]);
         Ok(SimpleError::new(s.to_string()))
     }
+}
+impl RespDecode for RespArray{
+    fn decode(buf:&mut BytesMut)->Result<Self,RespError>{
+        if buf.len() < 3 {
+            return Err(RespError::NotComplete);
+        }
+    }
+
+}
+/**
+ * 从给定的字节缓冲区中提取简单帧数据。
+ *
+ * 该函数旨在处理Redis响应协议中的简单字符串类型帧。它首先检查缓冲区是否足够长，然后确认缓冲区的前缀是否符合预期，
+ * 最后找到并返回简单字符串的结束位置。
+ *
+ * @param buf 待处理的字节缓冲区，应包含一个Redis简单字符串帧。
+ * @param prefix 预期的帧前缀，用于识别帧类型。
+ * @return 返回简单字符串帧的结束位置索引，如果帧不完整或类型不匹配，则返回错误。
+ */
+fn extract_simple_frame_data(buf: &[u8], prefix: &str) -> Result<usize, RespError> {
+    // 检查缓冲区长度是否小于3，即至少应包含前缀和CRLF
+    if buf.len() < 3 {
+        return Err(RespError::NotComplete);
+    }
+
+    // 检查缓冲区是否以预期的前缀开头
+    if !buf.starts_with(prefix.as_bytes()) {
+        return Err(RespError::InvalidFrameType(format!(
+            "expect: SimpleString({}), got: {:?}",
+            prefix, buf
+        )));
+    }
+
+    // 寻找CRLF的位置，即简单字符串的结束位置
+    let end = find_crlf(buf, 1).ok_or(RespError::NotComplete)?;
+
+    // 返回找到的结束位置
+    Ok(end)
+}
+
+// find nth CRLF in the buffer 在缓冲区中找到第 n 个 CRLF
+fn find_crlf(buf: &[u8], nth: usize) -> Option<usize> {
+    let mut count = 0;
+    for i in 1..buf.len() - 1 {
+        if buf[i] == b'\r' && buf[i + 1] == b'\n' {
+            count += 1;
+            if count == nth {
+                return Some(i);
+            }
+        }
+    }
+
+    None
 }
 #[cfg(test)]
 mod tests{
